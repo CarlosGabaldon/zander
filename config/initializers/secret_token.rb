9289fcc0400255351c5b576e7718c4e2a91bc8f3
Carlos Gabaldon <cgabaldon@gmail.com>
2010-09-26 09:33:19 -0700

    initial project creation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Zander::Application.config.secret_token = 'd069a22dfb948e2f9218b43552b95f8b3bdfa6daac20a47738dfeca394e2f8864d6ab023fa722177216d3693c1ff949a2e54e3b99ce1e010378e5b1d92e95749'
